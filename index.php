0db5b0d2f78c878dc5f112e4c0890e4246711cb3
Zane Durkin <zanedurkin@gmail.com>
2016-04-01 18:41:18 -0600

    This Challenge was hosted at hk1.zemptech.com (the one with the sloth image)

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head><title>Can't See Me</title></head>
+<body>
+<h1>No Search engine can see this password. Not even Google will recognize it.</h1>
+<p>But I know you're better than a robot. So While you search, enjoy this picture of a sloth. <br> They're fascinating creatures!</p>
+<img src="/etc/sloth.jpg" alt='SSSLLLLOOOTTTHHH!!!!!'><!-- image credits: http://news.bbc.co.uk/media/images/47242000/jpg/_47242706_dpp_0006.jpg-->
+</body>
+</html>
